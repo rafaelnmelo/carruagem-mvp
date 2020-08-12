ad16fbfc99fc7ad1c950d49719e856f2d83f600a
Rafael Melo <rafael.melo@mobicare.com.br>
2020-08-12 10:27:08 -0300

    initial commit

---
+++ Carruagem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Carruagem
+//
+//  Created by Rafael Nunes on 12/08/20.
+//  Copyright © 2020 Rafael Nunes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
