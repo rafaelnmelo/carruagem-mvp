aa0bb244166fe96066eb12dc35934f801966a917
Rafael Melo <rafaelnunesmelo@gmail.com>
2024-07-11 11:50:28 -0300

    Uniformizando navigation bar

--- Carruagem/AppDelegate.swift
+++ Carruagem/AppDelegate.swift
@@ -4,7 +4,28 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        setupBarAppearence()
         return true
+    }
+    
+    private func setupBarAppearence() {
+        let barAppearance = UINavigationBarAppearance()
+        let textAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        //BarButtons ItemColor
+        UIBarButtonItem.appearance().tintColor = .white
+        
+        //Collapsed TitleColor
+        barAppearance.titleTextAttributes = textAttributes
+        
+        //Large TitleColor
+        barAppearance.backgroundColor = .black
+        barAppearance.largeTitleTextAttributes = textAttributes
+        
+        if #available(iOS 13, *) {
+            UINavigationBar.appearance().standardAppearance = barAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = barAppearance
+        }
     }
 
 // MARK: UISCENE SESSION LIFE CYCLE
